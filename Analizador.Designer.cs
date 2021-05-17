0a4374c82fb76d8c60e0815ba684b09355e7fa71
Juan Baltierrez <juanbal2808@gmail.com>
2021-05-16 22:43:31 -0500

    Initial commit

---
+++ Analizador.Designer.cs
@@ -0,0 +1,199 @@
+﻿
+namespace AnalizadorLexico
+{
+    partial class Analizador
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rtxtLineNumber = new System.Windows.Forms.RichTextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.rtxtCodigo = new System.Windows.Forms.RichTextBox();
+            this.tabSalidas = new System.Windows.Forms.TabControl();
+            this.tpgErrores = new System.Windows.Forms.TabPage();
+            this.tpgSalida = new System.Windows.Forms.TabPage();
+            this.rtxtTokens = new System.Windows.Forms.RichTextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.cargarProgramaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.guardarProgramaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.iniciarAnalisisToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel1.SuspendLayout();
+            this.tabSalidas.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // rtxtLineNumber
+            // 
+            this.rtxtLineNumber.BackColor = System.Drawing.Color.White;
+            this.rtxtLineNumber.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.rtxtLineNumber.Cursor = System.Windows.Forms.Cursors.PanNE;
+            this.rtxtLineNumber.Dock = System.Windows.Forms.DockStyle.Left;
+            this.rtxtLineNumber.Font = new System.Drawing.Font("Consolas", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rtxtLineNumber.ForeColor = System.Drawing.Color.SteelBlue;
+            this.rtxtLineNumber.Location = new System.Drawing.Point(6, 6);
+            this.rtxtLineNumber.Name = "rtxtLineNumber";
+            this.rtxtLineNumber.ReadOnly = true;
+            this.rtxtLineNumber.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.None;
+            this.rtxtLineNumber.Size = new System.Drawing.Size(40, 431);
+            this.rtxtLineNumber.TabIndex = 0;
+            this.rtxtLineNumber.Text = "";
+            this.rtxtLineNumber.MouseDown += new System.Windows.Forms.MouseEventHandler(this.rtxtLineNumber_MouseDown);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Controls.Add(this.rtxtCodigo);
+            this.panel1.Controls.Add(this.rtxtLineNumber);
+            this.panel1.Location = new System.Drawing.Point(12, 35);
+            this.panel1.Name = "panel1";
+            this.panel1.Padding = new System.Windows.Forms.Padding(6);
+            this.panel1.Size = new System.Drawing.Size(816, 443);
+            this.panel1.TabIndex = 1;
+            // 
+            // rtxtCodigo
+            // 
+            this.rtxtCodigo.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.rtxtCodigo.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.rtxtCodigo.Font = new System.Drawing.Font("Consolas", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rtxtCodigo.Location = new System.Drawing.Point(46, 6);
+            this.rtxtCodigo.Margin = new System.Windows.Forms.Padding(10, 3, 3, 3);
+            this.rtxtCodigo.Name = "rtxtCodigo";
+            this.rtxtCodigo.Size = new System.Drawing.Size(764, 431);
+            this.rtxtCodigo.TabIndex = 1;
+            this.rtxtCodigo.Text = "";
+            this.rtxtCodigo.SelectionChanged += new System.EventHandler(this.rtxtCodigo_SelectionChanged);
+            this.rtxtCodigo.VScroll += new System.EventHandler(this.rtxtCodigo_VScroll);
+            this.rtxtCodigo.FontChanged += new System.EventHandler(this.rtxtCodigo_FontChanged);
+            this.rtxtCodigo.TextChanged += new System.EventHandler(this.rtxtCodigo_TextChanged);
+            // 
+            // tabSalidas
+            // 
+            this.tabSalidas.Controls.Add(this.tpgErrores);
+            this.tabSalidas.Controls.Add(this.tpgSalida);
+            this.tabSalidas.Location = new System.Drawing.Point(12, 484);
+            this.tabSalidas.Name = "tabSalidas";
+            this.tabSalidas.SelectedIndex = 0;
+            this.tabSalidas.Size = new System.Drawing.Size(816, 131);
+            this.tabSalidas.TabIndex = 2;
+            // 
+            // tpgErrores
+            // 
+            this.tpgErrores.Location = new System.Drawing.Point(4, 22);
+            this.tpgErrores.Name = "tpgErrores";
+            this.tpgErrores.Padding = new System.Windows.Forms.Padding(3);
+            this.tpgErrores.Size = new System.Drawing.Size(808, 105);
+            this.tpgErrores.TabIndex = 0;
+            this.tpgErrores.Text = "Lista de errores";
+            this.tpgErrores.UseVisualStyleBackColor = true;
+            // 
+            // tpgSalida
+            // 
+            this.tpgSalida.Location = new System.Drawing.Point(4, 22);
+            this.tpgSalida.Name = "tpgSalida";
+            this.tpgSalida.Padding = new System.Windows.Forms.Padding(3);
+            this.tpgSalida.Size = new System.Drawing.Size(808, 105);
+            this.tpgSalida.TabIndex = 1;
+            this.tpgSalida.Text = "Salida";
+            this.tpgSalida.UseVisualStyleBackColor = true;
+            // 
+            // rtxtTokens
+            // 
+            this.rtxtTokens.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.rtxtTokens.Location = new System.Drawing.Point(864, 35);
+            this.rtxtTokens.Name = "rtxtTokens";
+            this.rtxtTokens.Size = new System.Drawing.Size(428, 443);
+            this.rtxtTokens.TabIndex = 3;
+            this.rtxtTokens.Text = "";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.cargarProgramaToolStripMenuItem,
+            this.guardarProgramaToolStripMenuItem,
+            this.iniciarAnalisisToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1316, 24);
+            this.menuStrip1.TabIndex = 4;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // cargarProgramaToolStripMenuItem
+            // 
+            this.cargarProgramaToolStripMenuItem.Name = "cargarProgramaToolStripMenuItem";
+            this.cargarProgramaToolStripMenuItem.Size = new System.Drawing.Size(109, 20);
+            this.cargarProgramaToolStripMenuItem.Text = "Cargar programa";
+            // 
+            // guardarProgramaToolStripMenuItem
+            // 
+            this.guardarProgramaToolStripMenuItem.Name = "guardarProgramaToolStripMenuItem";
+            this.guardarProgramaToolStripMenuItem.Size = new System.Drawing.Size(116, 20);
+            this.guardarProgramaToolStripMenuItem.Text = "Guardar programa";
+            // 
+            // iniciarAnalisisToolStripMenuItem
+            // 
+            this.iniciarAnalisisToolStripMenuItem.Name = "iniciarAnalisisToolStripMenuItem";
+            this.iniciarAnalisisToolStripMenuItem.Size = new System.Drawing.Size(92, 20);
+            this.iniciarAnalisisToolStripMenuItem.Text = "Iniciar analisis";
+            // 
+            // Analizador
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1316, 627);
+            this.Controls.Add(this.rtxtTokens);
+            this.Controls.Add(this.tabSalidas);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Analizador";
+            this.Text = "Analizador Lexico";
+            this.Load += new System.EventHandler(this.Analizador_Load);
+            this.Resize += new System.EventHandler(this.Analizador_Resize);
+            this.panel1.ResumeLayout(false);
+            this.tabSalidas.ResumeLayout(false);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox rtxtLineNumber;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.RichTextBox rtxtCodigo;
+        private System.Windows.Forms.TabControl tabSalidas;
+        private System.Windows.Forms.TabPage tpgErrores;
+        private System.Windows.Forms.TabPage tpgSalida;
+        private System.Windows.Forms.RichTextBox rtxtTokens;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem cargarProgramaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem guardarProgramaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem iniciarAnalisisToolStripMenuItem;
+    }
+}
+
---
+++ Analizador.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AnalizadorLexico
+{
+    public partial class Analizador : Form
+    {
+        public Analizador()
+        {
+            InitializeComponent();
+        }
+
+        private void Analizador_Load(object sender, EventArgs e)
+        {
+            rtxtLineNumber.Font = rtxtCodigo.Font;
+            rtxtCodigo.Select();
+            AddLineNumbers();
+        }
+        public int getWidth()
+        {
+            int w = 25;
+            // get total lines of rtxtCodigo    
+            int line = rtxtCodigo.Lines.Length;
+
+            if (line <= 99)
+            {
+                w = 20 + (int)rtxtCodigo.Font.Size;
+            }
+            else if (line <= 999)
+            {
+                w = 30 + (int)rtxtCodigo.Font.Size;
+            }
+            else
+            {
+                w = 50 + (int)rtxtCodigo.Font.Size;
+            }
+
+            return w;
+        }
+        public void AddLineNumbers()
+        {
+            // create & set Point pt to (0,0)    
+            Point pt = new Point(0, 0);
+            // get First Index & First Line from rtxtCodigo    
+            int First_Index = rtxtCodigo.GetCharIndexFromPosition(pt);
+            int First_Line = rtxtCodigo.GetLineFromCharIndex(First_Index);
+            // set X & Y coordinates of Point pt to ClientRectangle Width & Height respectively    
+            pt.X = ClientRectangle.Width;
+            pt.Y = ClientRectangle.Height;
+            // get Last Index & Last Line from rtxtCodigo    
+            int Last_Index = rtxtCodigo.GetCharIndexFromPosition(pt);
+            int Last_Line = rtxtCodigo.GetLineFromCharIndex(Last_Index);
+            // set Center alignment to rtxtLineNumber    
+            rtxtLineNumber.SelectionAlignment = HorizontalAlignment.Center;
+            // set rtxtLineNumber text to null & width to getWidth() function value    
+            rtxtLineNumber.Text = "";
+            rtxtLineNumber.Width = getWidth();
+            // now add each line number to rtxtLineNumber upto last line    
+            for (int i = First_Line; i <= Last_Line + 2; i++)
+            {
+                rtxtLineNumber.Text += i + 1 + "\n";
+            }
+        }
+
+        private void Analizador_Resize(object sender, EventArgs e)
+        {
+            AddLineNumbers();
+        }
+
+        private void rtxtCodigo_SelectionChanged(object sender, EventArgs e)
+        {
+            Point pt = rtxtCodigo.GetPositionFromCharIndex(rtxtCodigo.SelectionStart);
+            if (pt.X == 1)
+            {
+                AddLineNumbers();
+            }
+        }
+
+        private void rtxtCodigo_VScroll(object sender, EventArgs e)
+        {
+
+            rtxtLineNumber.Text = "";
+            AddLineNumbers();
+            rtxtLineNumber.Invalidate();
+        }
+
+        private void rtxtCodigo_TextChanged(object sender, EventArgs e)
+        {
+            if (rtxtCodigo.Text == "")
+            {
+                AddLineNumbers();
+            }
+        }
+
+        private void rtxtCodigo_FontChanged(object sender, EventArgs e)
+        {
+            rtxtLineNumber.Font = rtxtCodigo.Font;
+            rtxtCodigo.Select();
+            AddLineNumbers();
+        }
+
+        private void rtxtLineNumber_MouseDown(object sender, MouseEventArgs e)
+        {
+            rtxtCodigo.Select();
+            rtxtLineNumber.DeselectAll();
+        }
+    }
+}
